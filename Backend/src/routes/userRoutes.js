a43e10dfc31711c0d06c7cce32b74b481957b889
Aman Gupta <amann4249@gmail.com>
2025-06-16 17:45:51 +0530

    health route

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -8,6 +8,13 @@ const router = express.Router()
 router.post("/signup", signUp)
 router.post("/login", loginUser)
 router.post("/logout", logoutUser)
+router.get('/health', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        message: 'Server is healthy',
+        timestamp: new Date().toISOString(),
+    });
+});
 
 router.get("/checkuser", validateToken, checkUser)
 
